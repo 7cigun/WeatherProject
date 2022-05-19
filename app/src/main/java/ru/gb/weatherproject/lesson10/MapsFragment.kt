be95d12569d280dbe29646cc32559f3988a70f7c
7cigun <7cigun@gmail.com>
2022-05-20 02:39:23 +0300

    Урок 10 Добавил поиск по названию

--- app/src/main/java/ru/gb/weatherproject/lesson10/MapsFragment.kt
+++ app/src/main/java/ru/gb/weatherproject/lesson10/MapsFragment.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.graphics.Color
+import android.location.Geocoder
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
@@ -19,6 +20,8 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
 import ru.gb.weatherproject.R
 import ru.gb.weatherproject.databinding.FragmentMapsMainBinding
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MapsFragment : Fragment() {
 
@@ -37,7 +40,7 @@ class MapsFragment : Fragment() {
                 requireContext(),
                 Manifest.permission.ACCESS_FINE_LOCATION
             ) == PackageManager.PERMISSION_GRANTED
-        ){
+        ) {
             map.isMyLocationEnabled = true
         }
 
@@ -51,14 +54,15 @@ class MapsFragment : Fragment() {
         markers.add(marker)
     }
 
-    private fun drawLine(){
+    private fun drawLine() {
         var previousBefore: Marker? = null
-        markers.forEach { current->
-            previousBefore?.let{  previous->
+        markers.forEach { current ->
+            previousBefore?.let { previous ->
                 map.addPolyline(
-                    PolylineOptions().add(previous.position,current.position)
+                    PolylineOptions().add(previous.position, current.position)
                         .color(Color.RED)
-                        .width(5f))
+                        .width(5f)
+                )
             }
             previousBefore = current
         }
@@ -100,5 +104,30 @@ class MapsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+        initView()
+    }
+
+    private fun initView() {
+        binding.buttonSearch.setOnClickListener() {
+            val searchText = binding.searchAddress.text.toString()
+            val geocoder = Geocoder(requireContext(), Locale.getDefault())
+            val results = geocoder.getFromLocationName(searchText, 1)
+            val location = LatLng(
+                results[0].latitude,
+                results[0].longitude
+            )
+
+            map.addMarker(
+                MarkerOptions()
+                    .position(location)
+                    .title(searchText)
+                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_map_marker))
+            )
+            map.moveCamera(
+                CameraUpdateFactory.newLatLngZoom(
+                    location, 10f
+                )
+            )
+        }
     }
 }
